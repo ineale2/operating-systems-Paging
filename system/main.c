7a7d231aedab609d19fdb0e68edf15f07e7ebf41
Isaac  Neale <ineale@xinu21.cs.purdue.edu>
2019-03-29 19:21:19 -0400

    first commit after unzip

---
+++ system/main.c
@@ -0,0 +1,26 @@
+#include <xinu.h>
+
+extern void page_policy_test(void);
+
+process	main(void)
+{
+  srpolicy(FIFO);
+
+  /* Start the network */
+  /* DO NOT REMOVE OR COMMENT BELOW */
+#ifndef QEMU
+  netstart();
+#endif
+
+  /*  TODO. Please ensure that your paging is activated 
+      by checking the values from the control register.
+  */
+
+  /* Initialize the page server */
+  /* DO NOT REMOVE OR COMMENT THIS CALL */
+  psinit();
+
+  page_policy_test();
+
+  return OK;
+}
